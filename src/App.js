fc934a7ece6600e036afbdebc518b1bdd3ee3463
King-of-57 <43877536+King-of-57@users.noreply.github.com>
2020-07-08 10:48:03 -0700

    Add files via upload
    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function App() {
+  return (
+      <div>
+          <div className="Calendar">
+            <Calendar/>
+          </div>
+      </div>
+  );
+}
+
+export default App;
